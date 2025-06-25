b8c5d8d47e5906f088a1f3c7a5aa4870358f53f8
Byungjin Lee <byungjin.dev@gmail.com>
2025-06-25 22:29:53 +0900

    fix: Build Error on Mac (Window only attribute)

--- src-tauri/src/setup.rs
+++ src-tauri/src/setup.rs
@@ -1,11 +1,10 @@
-use crate::{controllers::main_controller::MainController, sentiment_analyzer::OnnxSession};
 use crate::services::db::service::DBService;
 use crate::state::AppState;
+use crate::{controllers::main_controller::MainController, sentiment_analyzer::OnnxSession};
 use anyhow::{Context, Result};
-use tauri::path::BaseDirectory;
-use tauri::utils::acl::resolved;
 use std::path::PathBuf;
 use std::sync::Arc;
+use tauri::path::BaseDirectory;
 use tauri::{App, Manager};
 use tokio::sync::Mutex;
 
@@ -16,6 +15,7 @@ pub fn setup(app: &mut App) -> Result<()> {
 }
 
 /// 애플리케이션의 핵심 서비스(DB 등)를 초기화하고 AppState를 생성합니다.
+#[cfg(not(target_os = "windows"))]
 pub fn initialize_core_services(app: &App) -> Result<AppState> {
     // 1. 데이터베이스 경로 확보 및 폴더 생성
     let db_path = initialize_app_paths(app).context("Failed to initialize app paths")?;
@@ -30,8 +30,35 @@ pub fn initialize_core_services(app: &App) -> Result<AppState> {
         main_controller: Mutex::new(MainController::new()), // 추후 추가
     };
 
-    // ort 관련 설정
-    let resource_path = app.path().resolve("lib/onnxruntime.dll", BaseDirectory::Resource)?;
+    // ai models onnx 설정
+    let onnx_session =
+        OnnxSession::new().expect("Failed to initialize the sentiment analysis model.");
+
+    app.manage(onnx_session);
+
+    Ok(app_state)
+}
+
+#[cfg(target_os = "windows")]
+pub fn initialize_core_services(app: &App) -> Result<AppState> {
+    // 1. 데이터베이스 경로 확보 및 폴더 생성
+    let db_path = initialize_app_paths(app).context("Failed to initialize app paths")?;
+    println!("Database path: {:?}", db_path);
+
+    // 2. 데이터베이스 서비스 초기화
+    let db_service = initialize_db_service(&db_path).context("Failed to initialize DB service")?;
+
+    // 3. AppState 생성
+    let app_state = AppState {
+        db: Arc::new(db_service),
+        main_controller: Mutex::new(MainController::new()), // 추후 추가
+    };
+
+    // ort 관련 설정 - window 일때만
+
+    let resource_path = app
+        .path()
+        .resolve("lib/onnxruntime.dll", BaseDirectory::Resource)?;
 
     ort::init_from(resource_path.to_string_lossy()).commit()?;
 
