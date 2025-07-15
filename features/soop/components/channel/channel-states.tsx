633a4c5e7614b4b6e1176b164cdb4991ead8f6e8
byungjin-lee <byungjin.dev@gmail.com>
2025-07-15 20:53:01 +0900

    feat: Pretendard Font 추가

--- features/soop/components/channel/channel-states.tsx
+++ features/soop/components/channel/channel-states.tsx
@@ -1,3 +1,5 @@
+import { useChannelEdit } from "../../stores/channel-edit";
+
 export const LoadingState: React.FC = () => {
   return (
     <div className="flex justify-center py-8">
@@ -9,17 +11,26 @@ export const LoadingState: React.FC = () => {
 export const ErrorState: React.FC = () => {
   return (
     <div className="flex justify-center py-8">
-      <div className="text-red-500">
-        채널 목록을 불러오는데 실패했습니다.
-      </div>
+      <div className="text-red-500">채널 목록을 불러오는데 실패했습니다.</div>
     </div>
   );
 };
 
 export const EmptyState: React.FC = () => {
+  const { openCreate } = useChannelEdit();
+
   return (
-    <div className="flex justify-center py-8">
-      <div className="text-gray-500">등록된 채널이 없습니다.</div>
+    <div className="flex flex-col gap-y-2 py-8">
+      <div className="text-gray-500 text-center">채널이 없습니다.</div>
+      <div
+        onClick={openCreate}
+        className="p-1 px-2 rounded-lg cursor-pointer text-white bg-blue-500 w-fit mx-auto"
+      >
+        <div className="text-center">
+          <span className="text-xl mr-1">+</span>
+          <span className="text-md">추가</span>
+        </div>
+      </div>
     </div>
   );
 };
